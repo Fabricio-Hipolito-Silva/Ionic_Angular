d961e8b3dcc60a38e64b2d0ef7650b9780fcbb66
Fabricio Hipólito <fabriciohipvpaul@gmail.com>
2024-03-05 21:16:40 -0300

    Pagina nova com Ionic Generate, e Back Button

---
+++ src/app/teste/teste.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import { IonicModule } from '@ionic/angular';
+
+import { TestePageRoutingModule } from './teste-routing.module';
+
+import { TestePage } from './teste.page';
+import { AppComponent } from '../app.component';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    TestePageRoutingModule
+  ],
+  declarations: [TestePage]
+})
+export class TestePageModule {}
